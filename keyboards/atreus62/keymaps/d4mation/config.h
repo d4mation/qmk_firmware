6fac9aa76c5eedb59664b98e79364f29368238f1
Eric Defore <eric.defore@gmail.com>
2020-07-21 15:50:38 -0400

    Move documentation for config flags to Userspace

--- keyboards/atreus62/keymaps/d4mation/config.h
+++ keyboards/atreus62/keymaps/d4mation/config.h
@@ -1,7 +1,2 @@
-/* Enable "CMD Overlay" when using Windows */
 #undef CMD_WINDOWS_ENABLE
 #define CMD_WINDOWS_ENABLE true
-
-/* CTRL is in the position that you expect CMD to be in */
-#undef CMD_WINDOWS_DEFAULT_CTRL
-#define CMD_WINDOWS_DEFAULT_CTRL true
--- users/d4mation/config.h
+++ users/d4mation/config.h
@@ -1,13 +1,16 @@
 #define FORCE_NKRO
 
+/* Assume Windows-based Keymap for things like Macros */
 #ifndef WINDOWS_LAYOUT_IS_DEFAULT
     #define WINDOWS_LAYOUT_IS_DEFAULT true
 #endif
 
+/* Enable "CMD Overlay" when using Windows */
 #ifndef CMD_WINDOWS_ENABLE
     #define CMD_WINDOWS_ENABLE false
 #endif
 
+/* CTRL is in the position that you expect CMD to be in */
 #ifndef CMD_WINDOWS_DEFAULT_CTRL
     #define CMD_WINDOWS_DEFAULT_CTRL true
 #endif
